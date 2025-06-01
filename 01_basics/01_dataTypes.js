4f624015e52d7effdfd3f1ed36ae232ab7f8e0bc
Aritra Ghosh <aritraghosh184@gmail.com>
2025-06-01 20:26:35 +0000

    2nd additions

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"  // treat all JS code as newer version
+
+let name = "aritra"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => abho value assign nhi hua hai
+// symbol => unique
+// object
+
+console.log(typeof null) // output = object
+console.log(typeof undefined) // output = undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=144553
+let accountEmail= "ag@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState;
+
+// accountId==2 // not allowed
+accountEmail = "ss@gmail.com"
+accountPassword = "108801"
+accountCity = "Bengaluru"
+
+console.table ([accountId, accountEmail, accountPassword,accountCity, accountState])
