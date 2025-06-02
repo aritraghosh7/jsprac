a0e8f3c75f27196b29e50b41c4b5b79f9d673e77
Aritra Ghosh <aritraghosh184@gmail.com>
2025-06-02 07:11:35 +0000

    operations saved

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,3 +30,38 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber) // return string although it's looks like number
+
+
+
+// ***********Operations ***********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log (str3)
+
+console.log ("1" + 2);//12
+console.log (1 + "2");//12
+console.log ("1" + 2 +2);//122
+console.log (1+2+"2");//32
+console.log ((3+4)*5%3);
+
+console.log(+true);//1
+// console.log(true+);//Error
+console.log(+"");//0
+
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);//101
+
+
